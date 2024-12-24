5d660fdb6ec19a771db0fcb60bc6db7fba756039
ChatBear <adamaly.shiraz@outlook.fr>
2024-12-24 19:11:53 +0700

    first commit

---
+++ chatbear.io/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p> Didier des champs </p>;
+}
+
+export default App;
---
+++ chatbear.io/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
+export const Home = () => {
+  return (
+    <>
+      <Title>Didier des champs</Title>
+    </>
+  );
+};
